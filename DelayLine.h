60deb9ff42d91041dfbeb2463fd8814f7bdd6f9d
Matt Davison <mattd188@live.co.uk>
2023-12-31 15:28:52 +0000

    Created Delay Line class

---
+++ DelayLine.h
@@ -0,0 +1,26 @@
+/*
+
+DelayLine.h
+
+Author: Matt Davison
+Date: 31/12/2023
+
+*/
+
+template<typename T, unsigned long buffer_length>
+class DelayLine
+{
+public:
+
+    T step(T write_value)
+    {
+        T read_value = m_buffer[m_index];
+        m_buffer[m_index] = write_value;
+        m_index = (m_index + 1) % buffer_length;
+    }
+
+private:
+    T m_buffer[buffer_length]; 
+    unsigned long m_index = 0;
+
+};
