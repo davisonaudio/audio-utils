10cff677f91ed8716748acd19213bdf507ac70d7
Matt Davison <mattd188@live.co.uk>
2024-06-17 10:18:51 +0100

    Moved implementation of GoertzelAlgorithm methods

--- au_GoertzelAlgorithm.h
+++ au_GoertzelAlgorithm.h
@@ -34,34 +34,13 @@ public:
         }
     };
 
-    void setup(const SetupParameters& setup_parameters)
-    {
-        m_sample_rate = setup_parameters.sample_rate;
-        m_window_size_periods = setup_parameters.window_size_periods;
-        m_target_frequency = setup_parameters.target_frequency;
-        recalcCoefficients();
-    }
-
-    void setTargetFrequencyHz(sample_t target_frequency_hz)
-    {
-        m_target_frequency = target_frequency_hz;
-        recalcCoefficients();
-    }
-
-
-    void process(sample_t new_sample, QValues& q_vals)
-    {
-        sample_t q0 = new_sample + (m_coefficient * q_vals.q1) - q_vals.q2;
-        q_vals.q2 = q_vals.q1;
-        q_vals.q1 = q0;
-    }
+    void setup(const SetupParameters& setup_parameters);
+    void setTargetFrequencyHz(sample_t target_frequency_hz);
+    void process(sample_t new_sample, QValues& q_vals);
 
     
 
-    sample_t getMagnitudeQuick(QValues q_vals)
-    {
-        return sqrt( (q_vals.q1 * q_vals.q1) + (q_vals.q2 * q_vals.q2) - (m_coefficient * q_vals.q1 * q_vals.q2));
-    }
+    sample_t getMagnitudeQuick(QValues q_vals);
 
     struct ComplexPolarForm
     {
@@ -69,37 +48,22 @@ public:
         sample_t phase;
     };
 
-    ComplexPolarForm getComplexMagnitudeAndPhase(QValues q_vals)
-    {
-        ComplexPolarForm mag_and_phase;
-        sample_t real = q_vals.q1 - (q_vals.q2 * m_cosine_of_omega);
-        sample_t imaginary = q_vals.q2 * m_sine_of_omega;
-        mag_and_phase.magnitude = sqrt( (real * real) + (imaginary * imaginary);
-        mag_and_phase.phase = atan(imaginary / real);
-    }
+    ComplexPolarForm getComplexMagnitudeAndPhase(QValues q_vals);
+    
+ protected:   
+    int m_window_length_samples;
+    int m_window_size_periods;
 
-
-
+private:
     sample_t m_coefficient;
     sample_t m_sine_of_omega;
     sample_t m_cosine_of_omega;
 
-    int m_window_length_samples;
-    int m_window_size_periods;
-
     sample_t m_target_frequency;
     int m_sample_rate;
 
-private:
-    void recalcCoefficients()
-    {
-        sample_t omega = (2.0 * M_PI * m_target_frequency) / m_sample_rate;
-        m_coefficient = 2.0 * cos(omega);
-        m_sine_of_omega = sin(omega);
-        m_cosine_of_omega = cos(omega);
 
-        m_window_length_samples = (int) m_window_size_periods * (m_sample_rate / m_target_frequency);
-    }
+    void recalcCoefficients();
 };
 
 /*
@@ -183,3 +147,54 @@ private:
     }
 
 };
+
+
+
+/*
+ * GoertzelAlgorithm implementation
+ */
+
+void GoertzelAlgorithm::setup(const SetupParameters& setup_parameters)
+{
+    m_sample_rate = setup_parameters.sample_rate;
+    m_window_size_periods = setup_parameters.window_size_periods;
+    m_target_frequency = setup_parameters.target_frequency;
+    recalcCoefficients();
+}
+
+void GoertzelAlgorithm::setTargetFrequencyHz(sample_t target_frequency_hz)
+{
+    m_target_frequency = target_frequency_hz;
+    recalcCoefficients();
+}
+
+void GoertzelAlgorithm::process(sample_t new_sample, QValues& q_vals)
+{
+    sample_t q0 = new_sample + (m_coefficient * q_vals.q1) - q_vals.q2;
+    q_vals.q2 = q_vals.q1;
+    q_vals.q1 = q0;
+}
+
+sample_t GoertzelAlgorithm::getMagnitudeQuick(QValues q_vals)
+{
+    return sqrt( (q_vals.q1 * q_vals.q1) + (q_vals.q2 * q_vals.q2) - (m_coefficient * q_vals.q1 * q_vals.q2));
+}
+
+GoertzelAlgorithm::ComplexPolarForm GoertzelAlgorithm::getComplexMagnitudeAndPhase(QValues q_vals)
+{
+    ComplexPolarForm mag_and_phase;
+    sample_t real = q_vals.q1 - (q_vals.q2 * m_cosine_of_omega);
+    sample_t imaginary = q_vals.q2 * m_sine_of_omega;
+    mag_and_phase.magnitude = sqrt( (real * real) + (imaginary * imaginary);
+    mag_and_phase.phase = atan(imaginary / real);
+}
+
+void GoertzelAlgorithm::recalcCoefficients()
+{
+    sample_t omega = (2.0 * M_PI * m_target_frequency) / m_sample_rate;
+    m_coefficient = 2.0 * cos(omega);
+    m_sine_of_omega = sin(omega);
+    m_cosine_of_omega = cos(omega);
+
+    m_window_length_samples = (int) m_window_size_periods * (m_sample_rate / m_target_frequency);
+}
